ca3a8e245d744df043bad4cf3b134420711f7b76
Salvatore Laisa <moebiusmania@gmail.com>
2019-10-02 16:05:17 +0200

    begin adapt to UTU requirements

--- template/src/components/header/index.tsx
+++ template/src/components/header/index.tsx
@@ -3,22 +3,22 @@ import { Link } from "preact-router/match";
 import * as style from "./style.css";
 
 export default class Header extends Component {
-    public render() {
-        return (
-            <header class={style.header}>
-                <h1>Preact App</h1>
-                <nav>
-                    <Link activeClassName={style.active} href="/">
-                        Home
-                    </Link>
-                    <Link activeClassName={style.active} href="/profile">
-                        Me
-                    </Link>
-                    <Link activeClassName={style.active} href="/profile/john">
-                        John
-                    </Link>
-                </nav>
-            </header>
-        );
-    }
+  public render() {
+    return (
+      <header class={style.header}>
+        <h1>Preact App</h1>
+        <nav>
+          <Link activeClassName={style.active} href="/">
+            Home
+          </Link>
+          <Link activeClassName={style.active} href="/profile">
+            Me
+          </Link>
+          <Link activeClassName={style.active} href="/profile/john">
+            John
+          </Link>
+        </nav>
+      </header>
+    );
+  }
 }
